8842c9c2aed59283959fbe37526817777fd418ab
Connor Lundberg <connorlndbrg349@gmail.com>
2025-03-17 15:25:24 -0700

    Added better digital clock

--- clock.gd
+++ clock.gd
@@ -6,19 +6,20 @@ const START_TIME := 9 * 60 * 60  # 9:00 AM in seconds
 const END_TIME := 17 * 60 * 60   # 5:00 PM in seconds
 const WORKDAY_DURATION := END_TIME - START_TIME  # Total workday duration in seconds
 
-
-@onready var label = $Label
+@onready var hour_label = %HourLabel
+@onready var minute_label = %MinuteLabel
+@onready var meridian_label = %MeridianLabel
 @onready var timer = $Timer
 
 var _is_ended
  
 
-func convert_timer_to_clock(timer: Timer) -> String:
-	var elapsed_ratio := (timer.wait_time - timer.time_left) / timer.wait_time
-	var current_time := START_TIME + (WORKDAY_DURATION * elapsed_ratio)
-	var hours := int(current_time / 3600.0) % 24
-	var minutes := int((int(current_time) % 3600) / 60)
-	return "%d:%02d %s" % [hours if hours <= 12 else hours - 12, minutes, "AM" if hours < 12 else "PM"]
+func convert_timer_to_clock(time_left):
+	var elapsed_ratio = (timer.wait_time - time_left) / timer.wait_time
+	var current_time = START_TIME + (WORKDAY_DURATION * elapsed_ratio)
+	var hours = int(current_time / 3600.0) % 24
+	var minutes = int((int(current_time) % 3600) / 60)
+	return [hours if hours <= 12 else hours - 12, minutes, "AM" if hours < 12 else "PM"]
 
 
 func initialize():
@@ -44,11 +45,10 @@ func _process(_delta):
 
 
 func _update_time_left_to_live(time_left):
-	var minute = floor(time_left / 60)
-	var second = int(time_left) % 60 
-	label.text = "%02d:%02d" % [minute, second]
-	
-	label.text = convert_timer_to_clock(timer)
+	var time_parts = convert_timer_to_clock(time_left)
+	hour_label.text = "%d" % time_parts[0]
+	minute_label.text = "%02d" % time_parts[1]
+	meridian_label.text = "%s" % time_parts[2]
 
 
 func _on_timer_timeout():
