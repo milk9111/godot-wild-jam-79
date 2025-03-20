73f0cd3cb5041d51f002f0827ef51b2b55f08760
Connor Lundberg <connorlndbrg349@gmail.com>
2025-03-19 21:27:19 -0700

    Starting to add in a nicer performance review screen

--- performance_review.gd
+++ performance_review.gd
@@ -5,11 +5,13 @@ signal accepted_review
 @onready var success_count_label = %SuccessCountLabel
 @onready var failed_count_label = %FailedCountLabel
 @onready var nudged_count_label = %NudgedCountLabel
+@onready var day_label = %DayLabel
 
 func _ready():
 	visible = false
 
-func show_report(success_count, failed_count, nudged_count):
+func show_report(day, success_count, failed_count, nudged_count):
+	day_label.text = "DAY %d" % day
 	success_count_label.text = "Succeeded:\t\t\t%d" % success_count
 	failed_count_label.text = "Failed:\t\t\t\t\t%d" % failed_count
 	nudged_count_label.text = "Nudged:\t\t\t\t\t%d" % nudged_count
