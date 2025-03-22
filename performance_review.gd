a28bef8d11818ce03cdace23fb2ba04f10f06310
Connor Lundberg <connorlndbrg349@gmail.com>
2025-03-22 09:07:14 -0700

    Added days 3 and 4

--- performance_review.gd
+++ performance_review.gd
@@ -36,15 +36,110 @@ func show_report(day, success_count, failed_count, failures, bonus):
 			_day_one(success_count, failed_count, failures)
 		DayLevel.Day.TWO:
 			_day_two(success_count, failed_count, failures)
-		#DayLevel.Day.THREE:
-			#_day_three(success_count, failed_count, failures)
-		#DayLevel.Day.FOUR:
-			#_day_four(success_count, failed_count, failures)
+		DayLevel.Day.THREE:
+			_day_three(success_count, failed_count, failures)
+		DayLevel.Day.FOUR:
+			_day_four(success_count, failed_count, failures)
 		#DayLevel.Day.FIVE:
 			#_day_five(success_count, failed_count, failures)
 	
 	await animation_player.animation_finished
 	accept_button.show()
+
+
+func _day_four(success_count, failed_count, failures):
+	var failure_text = ""
+	if failures.size() > 0:
+		
+		failure_text = """
+
+It seems like there were a couple of unexpected outcomes that I'd like to discuss:
+
+[ul]
+%s
+[/ul]
+
+I trust you’ll take the necessary steps to ensure this doesn’t repeat.
+
+	""" % "\n".join(failures)
+			
+	var score_text = ""
+	if failed_count > 0 || success_count == 0:
+		score_text = "below KPI expectations. Please follow all written instructions tomorrow."
+	elif success_count > 0 && success_count < 25:
+		score_text = "meeting expectations. However, KPIers are extremely ambitious, so give it 110% tomorrow!"
+	else:
+		score_text = "exceeding expectations! Keep up the great work."
+	
+	review_text.parse_bbcode("""
+Date: 02/22/1999
+To: Whom it may concern
+From: Bob Thompson, Senior Regional Vice President
+Subject: Internship Performance Evaluation
+
+Thank you for your continued efforts at KPI Corp! Your hard work doesn’t go unnoticed. We’ve compiled your latest performance insights. Here is your daily Internship Performance Evaluation (IPE)
+
+[ul]
+ Total tasks assigned: %d
+ Completed tasks: %d
+ Growth opportunities: %d
+[/ul]
+%s
+Overall, we believe you are %s
+
+Thank you,
+
+Bob Thompson
+Senior Regional Vice President
+	""" % [success_count + failed_count, success_count, failed_count, failure_text, score_text])
+
+
+func _day_three(success_count, failed_count, failures):
+	var failure_text = ""
+	if failures.size() > 0:
+		
+		failure_text = """
+
+I’d like to talk through a few opportunities for improvement on this:
+
+[ul]
+%s
+[/ul]
+
+Let’s take this as a learning moment and move forward with more precision, mkay?
+
+	""" % "\n".join(failures)
+			
+	var score_text = ""
+	if failed_count > 0 || success_count == 0:
+		score_text = "below KPI expectations. Please follow all written instructions tomorrow."
+	elif success_count > 0 && success_count < 20:
+		score_text = "meeting expectations. However, KPIers are extremely ambitious, so give it 110% tomorrow!"
+	else:
+		score_text = "exceeding expectations! Keep up the great work."
+	
+	review_text.parse_bbcode("""
+Date: 02/21/1999
+To: Whom it may concern
+From: Bob Thompson, Branch Director
+Subject: Internship Performance Evaluation
+
+Thank you for your commitment to excellence at KPI Corp! Your contributions matter, and we’ve prepared your daily Internship Performance Evaluation (IPE) to reflect your progress.
+
+[ul]
+ Total tasks assigned: %d
+ Completed tasks: %d
+ Growth opportunities: %d
+[/ul]
+%s
+Overall, we believe you are %s
+
+Thank you,
+
+Bob Thompson
+Branch Director
+	""" % [success_count + failed_count, success_count, failed_count, failure_text, score_text])
+
 
 
 func _day_two(success_count, failed_count, failures):
@@ -77,7 +172,7 @@ To: Whom it may concern
 From: Bob Thompson, Office Manager
 Subject: Internship Performance Evaluation
 
-I hope you had a wonderful second with us at KPI Corp. The effort you put in is valuable and treasured! We've crunched the numbers, so here is your daily Internship Performance Evaluation (IPE)!
+I hope you had a wonderful second day with us at KPI Corp. The effort you put in is valuable and treasured! We've crunched the numbers, so here is your daily Internship Performance Evaluation (IPE)!
 
 [ul]
  Total tasks assigned: %d
